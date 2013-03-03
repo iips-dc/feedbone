9f547a51e7f31afe8ed727122977931feef865ce
Ankit <ankit.kul1890@gmail.com>
2013-03-03 13:45:12 +0530

    added hiding showing functionally in js

--- js/admin.js
+++ js/admin.js
@@ -1,25 +1,49 @@
 $(document).ready(function(){
-	
-		$('#showUserButton').click(function(){
-			$('#showUser').css('display','block');
-			$('#showStatus').css('display','none');
-			$('#generateReport').css('display','none');	
+		$('#createUserButton').click(function() {
+			$('#showUser').css('display', 'none');
+			$('#generateReport').css('display', 'none');
+			$('#showStatus').css('display', 'none');
+			$('#allocate').css('display', 'none');
+			$('#createUser').css('display', 'block');
 		});
 		
 		
-		$('#showStatusButton').click(function(){
-			$('#showStatus').css('display','block');
-			$('#showUser').css('display','none');
-			$('#generateReport').css('display','none');	
+		$('#showUserButton').click(function() {
+			
+			$('#showStatus').css('display', 'none');
+			$('#generateReport').css('display', 'none');
+			$('#showUser').css('display', 'block');	
+			$('#allocate').css('display', 'none');	
+			$('#createUser').css('display', 'none');			
+		});
+		
+		$('#showStatusButton').click(function() {
+			$('#showUser').css('display', 'none');
+			$('#generateReport').css('display', 'none');
+			$('#showStatus').css('display', 'block');
+			$('#allocate').css('display', 'none');
+			$('#createUser').css('display', 'none');
+		});
+		
+		$('#generateReportButton').click(function() {
+			$('#showUser').css('display', 'none');
+			$('#generateReport').css('display', 'block');
+			$('#showStatus').css('display', 'none');
+			$('#allocate').css('display', 'none');
+			$('#createUser').css('display', 'none');
+			
+		});
+		
+		$('#allocateButton').click(function() {
+			$('#showUser').css('display', 'none');
+			$('#generateReport').css('display', 'none');
+			$('#showStatus').css('display', 'none');
+			$('#allocate').css('display', 'block');
+			$('#createUser').css('display', 'none');
 		});
 		
 		
-		$('#generateReportButton').click(function(){
-			$('#generateReport').css('display','block');
-			$('#showStatus').css('display','none');
-			$('#showUser').css('display','none');	
-		});
 		
-		
-		});
 });
+
+
